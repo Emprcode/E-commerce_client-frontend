ee1a528a23a1d28ad5609cca1a1d05eaecfd6f88
Narayan Aryal <aryal.coding@gmail.com>
2023-03-26 12:53:53 +1100

    home page added

---
+++ src/components/page-components/ShopHere.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ShopHere = () => {
+  return (
+    <div>
+      <Row></Row>
+    </div>
+  );
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Button } from "react-bootstrap";
 import { MainLayout } from "../layout/MainLayout";
 
 const HomePage = () => {
   return (
     <MainLayout>
-      <div className="hero">
-        <div className="text-center text-light h1 p-5">
-          Unleash your inner rebel
+      <div className="hero ">
+        <div className="mt-5 d-flex flex-column gap-2 justify-content-center align-items-center">
+          <h1 className="fw-bold ">Unleash your inner rebel</h1>
+
+          <h5 className="mt-4 mb-5">
+            All latest streetware stuff available here.
+          </h5>
+
+          <Button variant="" className="btn btn-dark rounded fw-bold">
+            Shop Now!
+          </Button>
         </div>
       </div>
     </MainLayout>
