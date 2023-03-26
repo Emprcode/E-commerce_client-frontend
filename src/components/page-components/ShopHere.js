ef96517e866ee411137004462ad90dd6b792d712
Narayan Aryal <aryal.coding@gmail.com>
2023-03-26 15:44:51 +1100

    main shop here components added And now working on tranding part

--- src/components/page-components/ShopHere.js
+++ src/components/page-components/ShopHere.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 export const ShopHere = () => {
   return (
--- src/components/page-components/Tranding.js
+++ src/components/page-components/Tranding.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import shoes from "../../assets/shoes.png";
+import { CardComponents } from "../card/CardComponents";
 
 export const Tranding = () => {
   const [index, setIndex] = useState(0);
@@ -8,45 +10,82 @@ export const Tranding = () => {
     setIndex(selectedIndex);
   };
 
+  const products = [
+    {
+      title: "shoes",
+      img: shoes,
+      price: "$200",
+    },
+    {
+      title: "shoes",
+      img: shoes,
+      price: "$200",
+    },
+    {
+      title: "shoes",
+      img: shoes,
+      price: "$200",
+    },
+    {
+      title: "shoes",
+      img: shoes,
+      price: "$200",
+    },
+  ];
+
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
+    <div>
+      <h3 className=" p-5 text-center fw-bold">Trending</h3>
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
+      <Carousel
+        activeIndex={index}
+        onSelect={handleSelect}
+        className="bg-warning">
+        {/* {products?.length &&
+          products.map((prod) => (
+            <Carousel.Item key={prod.id}>
+              <CardComponents product={prod} />
+            </Carousel.Item>
+          ))} */}
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+        <Carousel.Item>
+          {products.map((item, i) => (
+            <CardComponents key={i} {...item} />
+          ))}
+        </Carousel.Item>
+        {/* <Carousel.Item>2</Carousel.Item>
+        <Carousel.Item>3</Carousel.Item>
+        <Carousel.Item>
+          <CardComponents />
+        </Carousel.Item> */}
+
+        {/* <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Second slide&bg=282c34"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=20232a"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item> */}
+      </Carousel>
+    </div>
   );
-}
+};
