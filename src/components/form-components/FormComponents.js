91d575c7c2b4f36ccc091e4fe134d6effd1b7a9f
Narayan Aryal <aryal.coding@gmail.com>
2023-04-03 11:58:12 +1000

    login page created

---
+++ src/components/form-components/FormComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+export const FormComponents = ({ label, ...item }) => {
+  return (
+    <Form.Group className="mb-3">
+      <Form.Label>{label}</Form.Label>
+      <Form.Control {...item} />
+    </Form.Group>
+  );
+};
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,45 @@
-import React from 'react'
-import { MainLayout } from '../layout/MainLayout'
-import { Container } from 'react-bootstrap'
-import { Membership } from '../components/page-components/Membership'
+import React from "react";
+import { MainLayout } from "../layout/MainLayout";
+import { Button, Container, Form } from "react-bootstrap";
+import { Membership } from "../components/page-components/Membership";
+import { FormComponents } from "../components/form-components/FormComponents";
 
 const Login = () => {
+  const InputFields = [
+    {
+      name: "email",
+      type: "email",
+      label: "Email",
+      placeholder: "Email",
+      required: "true",
+    },
+    {
+      name: "password",
+      type: "password",
+      label: "Password",
+      placeholder: "*****",
+      required: "true",
+    },
+  ];
   return (
     <MainLayout>
-        <Container>
+      <Container>
+        <div className="bg-warning text-center">
+          <h2>SIGN IN</h2>
+          <Form>
+            {InputFields.map((item, i) => (
+              <FormComponents key={i} {...item} />
+            ))}
 
-        </Container>
-        <Membership/>
-        
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </Container>
+      <Membership />
     </MainLayout>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
