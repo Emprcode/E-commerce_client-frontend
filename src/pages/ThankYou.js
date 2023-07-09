710cf1d0899f4c61fb6321e85f9c7d152cf70153
Narayan Aryal <aryal.coding@gmail.com>
2023-07-09 15:47:42 +1000

    thank you page created

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import { MainLayout } from '../layout/MainLayout'
-
+import React from "react";
+import { MainLayout } from "../layout/MainLayout";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const ThankYou = () => {
   return (
     <MainLayout>
-        Thank you!
-    </MainLayout>
-  )
-}
+      <Container className="text-center p-4 ">
+        <div className="shadow-md p-5">
+          <span className="text-success">
+            <AiOutlineCheckCircle size="70" />
+          </span>
 
-export default ThankYou
+          <div className="thankyou">Thank you</div>
+
+          <div className="h5 mt-5">Your order is successfully placed.</div>
+          <Link to="/" className="d-flex justify-content-center nav-link">
+            <div className="button mt-5">Back to Home</div>
+          </Link>
+        </div>
+      </Container>
+    </MainLayout>
+  );
+};
+
+export default ThankYou;
