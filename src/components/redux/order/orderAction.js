4b49c4c6a0c087454d4e8ebd26cea800be69de4e
Narayan Aryal <aryal.coding@gmail.com>
2024-08-16 12:59:34 +1000

    ui updated

--- src/components/redux/order/orderAction.js
+++ src/components/redux/order/orderAction.js
@@ -3,7 +3,6 @@ import { setOrders } from "./orderSlice";
 
 export const getOrdersAction = () => async (dispatch) => {
   const { status, result } = await getOrder();
-  console.log(result, status);
-  console.log(result);
+
   status === "success" && dispatch(setOrders(result));
 };
